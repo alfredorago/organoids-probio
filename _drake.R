c824d824dcbb4fb58480f2214a954ab9d994d128
Alfredo Rago <alfredo.rago@gmail.com>
2019-09-18 15:19:37 +0200

    Drake setup now functional

--- _drake.R
+++ _drake.R
@@ -4,7 +4,7 @@ library(drake)
 library(here)
 
 source(here("scripts/libraries.R"))
-source(here("scripts/functions.R"))
+#source(here("scripts/functions.R"))
 source(here("scripts/plan.R"))
 
 drake_config(plan = plan, verbose = 2)
--- scripts/plan.R
+++ scripts/plan.R
@@ -1,6 +1,6 @@
 ## Drake plan: decides what to run for the analysis.
 ## Note: it requires we call fastqc before the main analysis
-plan = (
+plan = drake_plan(
   # Find fastq and fastqc files
   fastq_dir = here("data/X201SC19060242-Z01-F001/raw_data/"),
   fastqc_dir = here("results/fastqc/")
