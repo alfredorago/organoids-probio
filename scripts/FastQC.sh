f9f4fed7f3367c95dac699a2c52aa923dfe83468
Alfredo Rago <alfredo.rago@gmail.com>
2019-09-18 16:48:22 +0200

    Annotation for the fastq scripts

--- scripts/FastQC.sh
+++ scripts/FastQC.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# run this by using the shell command
+# scripts/FastQC.sh > results/fastqc/log.txt 2>&1 &
+# disown
+# this launches it in the background and redirects the stdout and stderr logs
+# the disown command makes sure the process keeps running after logout
 
 echo "Initializing fastqc analysis"
 fastq_files="ls ./data/X201SC19060242-Z01-F001/raw_data/*.fq.gz"
