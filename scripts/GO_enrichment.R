3bb9912c9daccc9d69d17105bbf2cd432ac1dc76
Alfredo Rago <alfredo.rago@gmail.com>
2020-02-04 11:31:03 +0100

    Convert GO enrichment analyses to R script
    This version works by itself, next commit will integrate snakemake.

---
+++ scripts/GO_enrichment.R
@@ -0,0 +1,103 @@
+## Perform random-forest based GO enrichment analyses
+
+# Set library to local folder
+.libPaths(
+  c(
+    './packrat/lib/x86_64-pc-linux-gnu/3.6.1',
+    './packrat/lib-ext/x86_64-pc-linux-gnu/3.6.1',
+    './packrat/lib-R/x86_64-pc-linux-gnu/3.6.1')
+)
+
+
+library(here)
+library(magrittr)
+library(stringr)
+library(dplyr)
+
+library(DESeq2)
+library(biomaRt)
+library(GOexpress)
+library(GO.db)
+library(fdrtool)
+
+register(MulticoreParam(snakemake@threads))
+set.seed(42)
+
+## Import read counts and metadata
+culture_type_dataset <- readRDS(file = here("results/deseq/DE_2D_vs_3D.Rdata"))
+culture_type_results <- results(culture_type_dataset)
+metadata <- colData(culture_type_dataset)
+
+## Filter only expressed genes and apply vst
+culture_type_counts_filtered <-
+  culture_type_results %$%
+  is.na(padj) %>%
+  not() %>%
+  which() %>%
+  culture_type_dataset[.,] %>%
+  vst()
+
+## Row-normalize so genes have the same expression range
+culture_type_counts_scaled <-
+  culture_type_counts_filtered %>%
+  assay() %>%
+  apply(X = ., MARGIN = 1, FUN = scale, center = TRUE, scale = TRUE) %>%
+  t() %>%
+  set_rownames(., assay(culture_type_counts_filtered) %>% row.names %>% str_extract(string = ., pattern = "^[[:alnum:]]*")) %>%
+  set_colnames(., assay(culture_type_counts_filtered) %>% colnames)
+
+## Convert to expression set so we can input it to GOexpress
+culture_type_phenodata =
+  new("AnnotatedDataFrame",
+      data = as.data.frame(metadata),
+      varMetadata = colnames(metadata) %>% data.frame(labelDescription = ., row.names = .)
+  )
+
+expression_set <-
+  ExpressionSet(
+    assayData = culture_type_counts_scaled,
+    phenoData = culture_type_phenodata
+  )
+
+## Download GO reference tables
+GO_table = getBM(
+  attributes = c("ensembl_gene_id", "go_id"),
+  mart = useEnsembl(biomart = 'ensembl', dataset = 'hsapiens_gene_ensembl')
+) %>%
+  set_colnames(c("gene_id", "go_id"))
+
+GO_ontologies = select(
+  GO.db,
+  keytype = "GOID",
+  keys = unique(GO_table$go_id),
+  column = c("GOID", "DEFINITION", "ONTOLOGY")
+) %>%
+  set_colnames(c("go_id", "name", "namespace")) %>%
+  mutate(
+    namespace = factor(x = namespace, levels = c("CC", "BP", "MF"), labels = c("cellular_component", "biological_process", "molecular_function"))
+  )
+
+## Run random forest algorithm
+GO_results <- GO_analyse(
+  eSet = expression_set,
+  f = "treatment",
+  GO_genes = GO_table,
+  all_GO = GO_ontologies,
+  method = "rf"
+)
+
+## Filter low-count GOs, calculate p-values and correct them via fdr
+GO_pvalues =
+  GO_results %>%
+  subset_scores(total = 5) %>%
+  pValue_GO(N = 5)
+
+GO_qvalues =
+  GO_pvalues %$%
+  GO %>%
+  mutate(
+    q.val = fdrtool(p.val)$qval
+  ) %>%
+  dplyr::select(go_id, namespace_1003, total_count, data_count, q.val, p.val, ave_rank, ave_score, name_1006)
+
+write.csv(x = GO_qvalues, file = here("results/GO_enrichment/GO_results.csv"))
